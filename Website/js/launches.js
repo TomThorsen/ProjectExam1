a4dcf85914b28bb327472c864278713a5c79c77c
Unknown <wavetraxx@hotmail.com>
2019-02-19 13:36:46 +0100

    launch updater javascript v2

--- Website/js/launches.js
+++ Website/js/launches.js
@@ -1,7 +1,7 @@
 var launchObject;
 
 // get object from JSON
-fetch('https://launchlibrary.net/1.4/launch?next=5&lsp=SpX&mode=verbose')
+fetch('https://launchlibrary.net/1.4/launch?next=9&lsp=SpX&mode=verbose')
     .then(result => result.json())
     .then((res) => {
         createLaunches(res);
@@ -14,48 +14,34 @@ function createLaunches(result) {
     console.log(launchObject);
 
     for (var i = 0; i < result.launches.length; i++) {
-        // Header
-        var launchHeader = document.createElement('h1');
-        launchHeader.className = 'launchCompHeader';
-        // White Container
-        var launchContainerBox = document.createElement('div');
-        launchContainerBox.className = 'launchBox';
-        // Info Container
-        var launchContainer = document.createElement('div');
-        launchContainer.className = 'launchCont';
-        // Time Content
-        var launchTimeCont = document.createElement('div');
-        launchTimeCont.className = 'launchTime';
-        // Type Content
-        var launchTypeCont = document.createElement('div');
-        launchTypeCont.className = 'launchType';
-        // Company Content
-        var launchCompanyCont = document.createElement('div');
-        launchCompanyCont.className = 'launchCompany';
 
-        launchHeader.innerHTML += "Launch Company: " + result.launches[i].lsp.name;
+        var launchInfoCont = document.createElement('div');
+        launchInfoCont.className = 'launchInfo';
 
-        launchTimeCont.innerHTML += "<p><i>Time:</i> " + result.launches[i].windowstart; + "</p>";
+        var launchHeaderCont = document.createElement('h3');
+        launchHeaderCont.className = 'launchCompHeader';
 
-        launchTypeCont.innerHTML += "<p><i>Rocket Type: </i>" + result.launches[i].rocket.name; + "</p>";
-        launchTypeCont.innerHTML += "<p><i>Location: </i>" + result.launches[i].location.name; + "</p>";
-        launchTypeCont.innerHTML += "<p><i>Location: </i>" + result.launches[i].location.pads[0].name; + "</p>";
+        launchHeaderCont.innerHTML += "Launch Company: " + result.launches[i].lsp.name;
+
+        launchInfoCont.appendChild(launchHeaderCont);
+
+        launchInfoCont.innerHTML += '<a class="launchImgLink" href="' + result.launches[i].rocket.imageURL + '">' + '<img src="' + result.launches[i].rocket.imageURL + '"' + 'alt="' + result.launches[i].rocket.name + '"' + 'class="launchImage">';
+
+        launchInfoCont.innerHTML += "<p>Time: " + result.launches[i].windowstart; + "</p>";
+
+        launchInfoCont.innerHTML += "<p>Rocket Type: " + result.launches[i].rocket.name; + "</p>";
+        launchInfoCont.innerHTML += "<p>Location: " + result.launches[i].location.name; + "</p>";
+        launchInfoCont.innerHTML += "<p>Location: " + result.launches[i].location.pads[0].name; + "</p>";
 
         if (result.launches[i].missions[0] === undefined) {
-            launchCompanyCont.innerHTML += "<p><i>Payload Name:</i> " + "N/A" + "</p>";
-            launchCompanyCont.innerHTML += "<p><i>Payload Type:</i> " + "N/A" + "</p>";
+            launchInfoCont.innerHTML += "<p>Payload Name: " + "N/A" + "</p>";
+            launchInfoCont.innerHTML += "<p>Payload Type: " + "N/A" + "</p>";
         } else {
-            launchCompanyCont.innerHTML += "<p><i>Payload Name:</i> " + result.launches[i].missions[0].name; + "</p>";
-            launchCompanyCont.innerHTML += "<p><i>Payload Type:</i> " + result.launches[i].missions[0].typeName; + "</p>";
+            launchInfoCont.innerHTML += "<p>Payload Name: " + result.launches[i].missions[0].name; + "</p>";
+            launchInfoCont.innerHTML += "<p>Payload Type: " + result.launches[i].missions[0].typeName; + "</p>";
         }
-        document.getElementById('LaunchScriptContainer').appendChild(launchHeader);
-        document.getElementById('LaunchScriptContainer').appendChild(launchContainerBox);
-        document.getElementById('LaunchScriptContainer').appendChild(launchContainer);
-        launchContainerBox.appendChild(launchContainer);
-        launchContainer.appendChild(launchTimeCont);
-        launchContainer.appendChild(launchTypeCont);
-        launchContainer.appendChild(launchCompanyCont);
+
+        document.getElementById('LaunchScriptContainer').appendChild(launchInfoCont);
 
     }
 }
-
