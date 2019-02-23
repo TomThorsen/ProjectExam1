a418ae708514aa763696ce96372807b38783308a
Unknown <wavetraxx@hotmail.com>
2019-02-23 14:19:20 +0100

    Final polish

--- Website/js/launches.js
+++ Website/js/launches.js
@@ -43,22 +43,67 @@ function createLaunches(result) {
         launchInfoCont.className = 'launchInfo';
         var launchHeaderCont = document.createElement('h3');
         launchHeaderCont.className = 'launchCompHeader';
+        var launchTable = document.createElement('table');
+        launchTable.className = 'launchTable';
+
         // populate information
-        launchHeaderCont.innerHTML += "Launch Company: " + result.launches[i].lsp.name;
+        launchHeaderCont.innerHTML += "Launch Company: " + '<br>' + result.launches[i].lsp.name;
         launchInfoCont.appendChild(launchHeaderCont);
+
         launchInfoCont.innerHTML += '<a class="launchImgLink" href="' + result.launches[i].rocket.imageURL + '">' + '<img src="' + result.launches[i].rocket.imageURL + '" ' + 'alt="' + result.launches[i].rocket.name + '" ' + 'class="launchImage">' + '</a>';
-        launchInfoCont.innerHTML += "<p>Time: " + result.launches[i].windowstart; + "</p>";
-        launchInfoCont.innerHTML += "<p>Rocket Type: " + result.launches[i].rocket.name; + "</p>";
-        launchInfoCont.innerHTML += "<p>Location: " + result.launches[i].location.name; + "</p>";
-        launchInfoCont.innerHTML += "<p>Location: " + result.launches[i].location.pads[0].name; + "</p>";
+
+        var launchTableRowTime = document.createElement('tr');
+        launchTableRowTime.className = 'launchTableRowTime';
+        launchTableRowTime.innerHTML += "<th>Time:</th>";
+        launchTableRowTime.innerHTML += "<td>" + result.launches[i].windowstart + "</td>";
+        launchTable.appendChild(launchTableRowTime);
+        launchInfoCont.appendChild(launchTable);
+
+        var launchTableRowType = document.createElement('tr');
+        launchTableRowType.className = 'launchTableRowType';
+        launchTableRowType.innerHTML += "<th>Rocket Type:</th>";
+        launchTableRowType.innerHTML += "<td>" + result.launches[i].rocket.name + "</td>";
+        launchTable.appendChild(launchTableRowType);
+        launchInfoCont.appendChild(launchTable);
+
+        var launchTableRowLocation = document.createElement('tr');
+        launchTableRowLocation.className = 'launchTableRowLocation';
+        launchTableRowLocation.innerHTML += "<th>Location:</th>";
+        launchTableRowLocation.innerHTML += "<td>" + result.launches[i].location.pads[0].name; + "</td>";
+        launchTable.appendChild(launchTableRowLocation);
+        launchInfoCont.appendChild(launchTable);
+
         // if launch mission is unavailable display N/A
         if (result.launches[i].missions[0] === undefined) {
-            launchInfoCont.innerHTML += "<p>Payload Name: " + "N/A" + "</p>";
-            launchInfoCont.innerHTML += "<p>Payload Type: " + "N/A" + "</p>";
+            var launchTableRowPayloadName = document.createElement('tr');
+            launchTableRowPayloadName.className = 'launchTableRowPayloadName';
+            launchTableRowPayloadName.innerHTML += "<th>Load Name:</th>";
+            launchTableRowPayloadName.innerHTML += "<td>N/A</td>";
+            launchTable.appendChild(launchTableRowPayloadName);
+            launchInfoCont.appendChild(launchTable);
+
+            var launchTableRowPayloadType = document.createElement('tr');
+            launchTableRowPayloadType.className = 'launchTableRowPayloadType';
+            launchTableRowPayloadType.innerHTML += "<th>Load Type:</th>";
+            launchTableRowPayloadType.innerHTML += "<td>N/A</td>";
+            launchTable.appendChild(launchTableRowPayloadType);
+            launchInfoCont.appendChild(launchTable);
         } else {
-            launchInfoCont.innerHTML += "<p>Payload Name: " + result.launches[i].missions[0].name; + "</p>";
-            launchInfoCont.innerHTML += "<p>Payload Type: " + result.launches[i].missions[0].typeName; + "</p>";
+            var launchTableRowPayloadName = document.createElement('tr');
+            launchTableRowPayloadName.className = 'launchTableRowPayloadName';
+            launchTableRowPayloadName.innerHTML += "<th>Load Name:</th>";
+            launchTableRowPayloadName.innerHTML += "<td>" + result.launches[i].missions[0].name; + "</td>";
+            launchTable.appendChild(launchTableRowPayloadName);
+            launchInfoCont.appendChild(launchTable);
+
+            var launchTableRowPayloadType = document.createElement('tr');
+            launchTableRowPayloadType.className = 'launchTableRowPayloadType';
+            launchTableRowPayloadType.innerHTML += "<th>Load Type:</th>";
+            launchTableRowPayloadType.innerHTML += "<td>" + result.launches[i].missions[0].typeName; + "</td>";
+            launchTable.appendChild(launchTableRowPayloadType);
+            launchInfoCont.appendChild(launchTable);
         }
+
         document.getElementById('LaunchScriptContainer').appendChild(launchInfoCont);
     }
 }
